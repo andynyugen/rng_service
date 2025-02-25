2fd019fd7cdbf239a5155cb2aec8c9360941ef14
Andy Nguyen <andynguyen@Andys-MacBook-Pro.local>
2025-02-24 22:05:44 -0800

    For Nick Vu

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
 import time
 import re
+import sys
+
+
+def write_error():
+    # Write error to csv file
+    with open("data.csv", "w") as file:
+        file.write("ERROR!")
+    return
 
 def read_request():
     # Open data.txt
@@ -11,7 +19,14 @@ def read_request():
     # Print error if no data was provided
     if not data:
         print("ERROR: No data provided")
+        return "error"
     
+    # Write ERROR if the other program wrote incorrect format
+    elif ',' not in data:
+        print("ERROR: Did not write correct format! Try again!")
+        write_error()
+        return "error"
+        
     # Else tokenize the csv line string
     else:
         tokens = re.split(",", data)
@@ -37,33 +52,41 @@ def main():
     # Sleep for 3 seconds
     for i in range(3):
         print(".")
-        time.sleep(1)
+        time.sleep(2)
 
     # Read request from file "data.csv"
     tokens = read_request()
+    
+    # If failed to read csv line from file, exit program
+    if tokens == "error":
+        sys.exit()
 
-    # Store the tokens in specific variables
-    number_type = tokens[0] # string (int or float)
-    minimum = float(tokens[1]) # Minimum bound
-    maximum = float(tokens[2]) # Maximum bound
-    decimal_place = int(tokens[3]) # Decimal places
+    # Else, continue
+    else: 
+        # Store the tokens in specific variables
+        number_type = tokens[0] # string (int or float)
+        minimum = float(tokens[1]) # Minimum bound
+        maximum = float(tokens[2]) # Maximum bound
+        decimal_place = int(tokens[3]) # Decimal places
 
 
-    # Generate random number using tokens
-            
-    # If user requested integer
-    if (number_type == "int"):
-        # Convert min and max to int user requested integer
-        # Generate random number
-        rand_number = random.randint(int(minimum), int(maximum))
-            
-    # Else if, user requested a float
-    elif (number_type == "float"):
-        # Generate random number
-        rand_number = random.uniform(minimum, maximum)
+        # Generate random number using tokens
+                
+        # If user requested integer
+        if (number_type == "int"):
+            # Convert min and max to int user requested integer
+            # Generate random number
+            rand_number = random.randint(int(minimum), int(maximum))
+                
+        # Else if, user requested a float
+        elif (number_type == "float"):
+            # Generate random number
+            rand_number = random.uniform(minimum, maximum)
 
-    # Write random number into "data.csv"
-    write_rand_number(rand_number, number_type, decimal_place)
+        # Write random number into "data.csv"
+        write_rand_number(rand_number, number_type, decimal_place)
+        # Alert user that number has been written to file
+        print("Number has successfully been generated and written to data.csv.")
 
 if __name__ == "__main__":
     main()
